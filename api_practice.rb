b06ae354be228080d1d355bdf060eeb12bc0584f
Randy Rodriguez <Randyr2125@msn.com>
2021-05-02 21:47:15 -0400

    finished the while loops practice problems

---
+++ api_practice.rb
@@ -0,0 +1,70 @@
+# Make a Ruby script to display the current forecast (temperature min/max, description, wind speed, etc.)
+# Some features to add to your weather app:
+# Ask the user for the name of a city to forecast
+# Ask the user for preferred temperature units (Celcius or Farenheit)
+# Write the app in a loop (once you show the forecast, the user can keep entering different cities until they choose to quit the program)
+# Bonus:
+# Make a separate web request to display the 5 day forecast: https://openweathermap.org/forecast5
+# Make a separate web request to display other data (minute, hourly, historical: https://openweathermap.org/api/one-call-api
+# Allow the user to enter a city, and tell them how much warmer/colder it will be tomorrow than right now (this will involve multiple web requests)
+# Note:
+# Don’t use git and GitHub to save versions of your code during this exercise! (We’ll take a look at how to store API keys securely in a bit)
+# white_check_mark
+# eyes
+# raised_hands
+
+
+# 8:26
+# (reference code: https://github.com/actualize-chi-2021-03/practice_ruby/blob/main/weather-app.rb)
+
+
+# api_key = "0aa7e3efcce7992cbb076f90265a46a6"
+# # city = "bronx"
+
+# puts "Weather app:"
+# response = HTTP.get("https://api.openweathermap.org/data/2.5/weather?q=#{city}&units=imperial&appid=#{api_key}&units=imperial")
+
+# weather_data = response.parse
+# # pp weather_data
+# description = weather_data["weather"][0]["description"]
+# temp_min = weather_data["main"]["temp_min"]
+# temp_max = weather_data["main"]["temp_max"]
+# wind_speed = weather_data["wind"]["speed"]
+
+# p temp_min
+# p temp_max
+# p wind_speed
+# p description
+
+
+require "http"
+
+def get_weather()
+  puts "Name a city:"
+  city = gets.chomp
+  # api_key = "0aa7e3efcce7992cbb076f90265a46a6"
+  response = HTTP.get("https://api.openweathermap.org/data/2.5/weather?q=#{city}&units=imperial&appid=#{ENV["OPEN_WEATHER_API_KEY"]}&units=imperial")
+  city_arr = []
+  city_arr << city
+  weather_data = response.parse
+  description = weather_data["weather"][0]["description"]
+  temp_min = weather_data["main"]["temp_min"]
+  temp_max = weather_data["main"]["temp_max"]
+  wind_speed = weather_data["wind"]["speed"]
+  pp weather_data
+  p "______________________________________-"
+  index = 0
+  while index < city_arr.length
+    puts "Enter another city:"
+    other_city = gets.chomp
+    city_arr << other_city
+    index +=1
+    system "clear"
+    response = HTTP.get("https://api.openweathermap.org/data/2.5/weather?q=#{city_arr[index]}&units=imperial&appid=#{ENV["OPEN_WEATHER_API_KEY"]}&units=imperial")
+    weather_data = response.parse
+    pp weather_data
+  end
+  
+end
+
+get_weather()
--- practice.rb
+++ practice.rb
@@ -1,4 +1,10 @@
 # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+# num = 10
+# if num == 10
+#   p "0"
+# else
+#   p "-1"
+# end
 # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
 # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
 # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
@@ -7,7 +13,114 @@
 # 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
 # 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
 # 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
-# 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
+# 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints  3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
 # SOLUTIONS: https://gist.github.com/peterxjang/aee70f966f0f725609eee89b06e8a6c0
 
+
+# 1. Write a while loop to print the numbers 1 through 10.
+# num = 0
+# while true
+#   if num == 10
+#     break
+#   else
+#   p num += 1
+#   end
+# end
+
+
+# 2. Write a while loop that prints the word "hello" 5 times.
+
+# num = 0
+# while num < 5
+
+# p "hello"
+#   num += 1
+# end
+# 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+
+# while true
+#   puts "enter a word: (if you enter stop, I will stop asking)"
+#   usr_input = gets.chomp
+#   if usr_input == "stop"
+#     break
+#   else
+#   puts "enter a word: (if you enter stop, I will stop asking)"
+#   usr_input = gets.chomp
+#   end
+# end
+
+# 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+
+# num = 0
+
+# while num < 100
+# num = num + 5
+# p num
+# end
+
+# 5. Write a while loop that prints the number 9000 ten times.
+
+# output = 1
+
+# while output < 2
+# numstr = "9000 "
+#   p numstr * 10
+
+# end
+
+# 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+
+# puts "Enter a number: (wont stop until num is greater than 10)"
+# input = gets.chomp
+
+# while true
+#   if input.to_i > 10 
+#     break
+#   else
+#     puts "Enter a number: (wont stop until num is greater than 10)"
+#     input = gets.chomp 
+#   end
+# end
+
+# 7. Write a while loop that prints the numbers 50 to 70.
+
+# num = 49
+
+# while num < 70
+
+#   p num += 1
+# end
+
+# 8. Write a while loop that prints the phrase "Around the world" 144 times.
+
+# num = 1
+# while num < 145
+#   if num == 145
+#     break
+#   else 
+#     puts "#{num} Around the world"
+#   end
+#   num += 1
+# end
+
+# 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
+# while true 
+# puts "Enter a word with less than 5 words:"
+# input = gets.chomp
+# if input.length > 5
+#   break
+# else
+#   puts "Enter a word with less than 5 words:"
+#   input = gets.chomp
+# end
+# end
+
+# 10. Write a while loop that prints the even numbers from 2 to 40.
+# num = 2
+# while num < 40
+
+#   p num = num + 2
+# end
+
+# SOLUTIONS: https://gist.github.com/peterxjang/c4ec0e0f8f6e123d65ada9bf3100068b
 
